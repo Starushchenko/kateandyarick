968ac96df361f8db8b3658d69ed1fd197be4f0df
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2019-05-06 09:29:21 +0300

    Проект инициализирован. Добавлены блоки интро и демоменю

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 "use strict";
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 jQuery(document).ready(function () {
@@ -1828,7 +1834,189 @@ $(document).ready(function () {
 
 function closePopup() {
   $.magnificPopup.close();
-}
+} //
+// these easing functions are based on the code of glsl-easing module.
+// https://github.com/glslify/glsl-easings
+//
+
+
+var ease = {
+  exponentialIn: function exponentialIn(t) {
+    return t == 0.0 ? t : Math.pow(2.0, 10.0 * (t - 1.0));
+  },
+  exponentialOut: function exponentialOut(t) {
+    return t == 1.0 ? t : 1.0 - Math.pow(2.0, -10.0 * t);
+  },
+  exponentialInOut: function exponentialInOut(t) {
+    return t == 0.0 || t == 1.0 ? t : t < 0.5 ? +0.5 * Math.pow(2.0, 20.0 * t - 10.0) : -0.5 * Math.pow(2.0, 10.0 - t * 20.0) + 1.0;
+  },
+  sineOut: function sineOut(t) {
+    var HALF_PI = 1.5707963267948966;
+    return Math.sin(t * HALF_PI);
+  },
+  circularInOut: function circularInOut(t) {
+    return t < 0.5 ? 0.5 * (1.0 - Math.sqrt(1.0 - 4.0 * t * t)) : 0.5 * (Math.sqrt((3.0 - 2.0 * t) * (2.0 * t - 1.0)) + 1.0);
+  },
+  cubicIn: function cubicIn(t) {
+    return t * t * t;
+  },
+  cubicOut: function cubicOut(t) {
+    var f = t - 1.0;
+    return f * f * f + 1.0;
+  },
+  cubicInOut: function cubicInOut(t) {
+    return t < 0.5 ? 4.0 * t * t * t : 0.5 * Math.pow(2.0 * t - 2.0, 3.0) + 1.0;
+  },
+  quadraticOut: function quadraticOut(t) {
+    return -t * (t - 2.0);
+  },
+  quarticOut: function quarticOut(t) {
+    return Math.pow(t - 1.0, 3.0) * (1.0 - t) + 1.0;
+  }
+};
+/**
+ * navigation.js
+ * ! NAMESPACES HAS BEEN CHANGED
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2017, Codrops
+ * http://www.codrops.com
+ */
+
+var ShapeOverlays =
+/*#__PURE__*/
+function () {
+  function ShapeOverlays(elm) {
+    _classCallCheck(this, ShapeOverlays);
+
+    this.elm = elm;
+    this.path = elm.querySelectorAll('path');
+    this.numPoints = 2;
+    this.duration = 600;
+    this.delayPointsArray = [];
+    this.delayPointsMax = 0;
+    this.delayPerPath = 200;
+    this.timeStart = Date.now();
+    this.isOpened = false;
+    this.isAnimating = false;
+  }
+
+  _createClass(ShapeOverlays, [{
+    key: "toggle",
+    value: function toggle() {
+      this.isAnimating = true;
+
+      for (var i = 0; i < this.numPoints; i++) {
+        this.delayPointsArray[i] = 0;
+      }
+
+      if (this.isOpened === false) {
+        this.open();
+      } else {
+        this.close();
+      }
+    }
+  }, {
+    key: "open",
+    value: function open() {
+      this.isOpened = true;
+      this.elm.classList.add('is-opened');
+      this.timeStart = Date.now();
+      this.renderLoop();
+    }
+  }, {
+    key: "close",
+    value: function close() {
+      this.isOpened = false;
+      this.elm.classList.remove('is-opened');
+      this.timeStart = Date.now();
+      this.renderLoop();
+    }
+  }, {
+    key: "updatePath",
+    value: function updatePath(time) {
+      var points = [];
+
+      for (var i = 0; i < this.numPoints; i++) {
+        var thisEase = this.isOpened ? i == 1 ? ease.cubicOut : ease.cubicInOut : i == 1 ? ease.cubicInOut : ease.cubicOut;
+        points[i] = thisEase(Math.min(Math.max(time - this.delayPointsArray[i], 0) / this.duration, 1)) * 100;
+      }
+
+      var str = '';
+      str += this.isOpened ? "M 0 0 V ".concat(points[0], " ") : "M 0 ".concat(points[0], " ");
+
+      for (var i = 0; i < this.numPoints - 1; i++) {
+        var p = (i + 1) / (this.numPoints - 1) * 100;
+        var cp = p - 1 / (this.numPoints - 1) * 100 / 2;
+        str += "C ".concat(cp, " ").concat(points[i], " ").concat(cp, " ").concat(points[i + 1], " ").concat(p, " ").concat(points[i + 1], " ");
+      }
+
+      str += this.isOpened ? "V 0 H 0" : "V 100 H 0";
+      return str;
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      if (this.isOpened) {
+        for (var i = 0; i < this.path.length; i++) {
+          this.path[i].setAttribute('d', this.updatePath(Date.now() - (this.timeStart + this.delayPerPath * i)));
+        }
+      } else {
+        for (var i = 0; i < this.path.length; i++) {
+          this.path[i].setAttribute('d', this.updatePath(Date.now() - (this.timeStart + this.delayPerPath * (this.path.length - i - 1))));
+        }
+      }
+    }
+  }, {
+    key: "renderLoop",
+    value: function renderLoop() {
+      var _this = this;
+
+      this.render();
+
+      if (Date.now() - this.timeStart < this.duration + this.delayPerPath * (this.path.length - 1) + this.delayPointsMax) {
+        requestAnimationFrame(function () {
+          _this.renderLoop();
+        });
+      } else {
+        this.isAnimating = false;
+      }
+    }
+  }]);
+
+  return ShapeOverlays;
+}();
+
+(function () {
+  var elmHamburger = document.querySelector('.hamburger');
+  var gNavItems = document.querySelectorAll('.global-menu__item');
+  var elmOverlay = document.querySelector('.shape-overlays');
+  var overlay = new ShapeOverlays(elmOverlay);
+  elmHamburger.addEventListener('click', function () {
+    if (overlay.isAnimating) {
+      return false;
+    }
+
+    overlay.toggle();
+
+    if (overlay.isOpened === true) {
+      elmHamburger.classList.add('is-opened-navi');
+
+      for (var i = 0; i < gNavItems.length; i++) {
+        gNavItems[i].classList.add('is-opened');
+      }
+    } else {
+      elmHamburger.classList.remove('is-opened-navi');
+
+      for (var i = 0; i < gNavItems.length; i++) {
+        gNavItems[i].classList.remove('is-opened');
+      }
+    }
+  });
+})();
 
 jQuery(document).ready(function ($) {
   var tabs = $('.tabs');
